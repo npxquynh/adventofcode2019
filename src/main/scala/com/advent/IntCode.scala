0dfc5d2db0f14a7fc40f1464a56c32b5107ee60c
Quynh Nguyen <quynh.nguyen@soundcloud.com>
2019-12-11 22:07:21 +0100

    Day 5a: WIP

--- src/main/scala/com/advent/IntCode.scala
+++ src/main/scala/com/advent/IntCode.scala
@@ -1,16 +1,37 @@
 package com.advent
+import com.advent.Mode.Position
 
+sealed trait Mode
+
+object Mode {
+  case object Position extends Mode
+  case object Immediate extends Mode
+
+  def build(mode: Int = 0): Mode = {
+    if (mode == 0) Position
+    else Immediate
+  }
+}
+class Instruction(opCode: Int, firstMode: Mode, secondMode: Mode, thirdMode: Mode) {
+
+  def apply(instruction: String): Instruction = {
+    val opCode = instruction.takeRight(2).toInt
+
+    new Instruction(2, Position, Position, Position)
+  }
+}
 class IntCode(input: Array[Int]) {
   val Step = 4
 
   def execute(): Array[Int] = {
 
-    def helper(binaryOp: (Int, Int) => Int, baseIndex: Int) = 
+
+    def helper(binaryOp: (Int, Int) => Int, baseIndex: Int) =
       update(get(baseIndex + 3), binaryOp(get(get(baseIndex + 1)), get(get(baseIndex + 2))))
 
     val addMethod: (Int, Int) => Int = _ + _
     val multiplyMethod: (Int, Int) => Int = _ * _
-    
+
     var currentIndex = 0
     var opCode = get(currentIndex)
 
